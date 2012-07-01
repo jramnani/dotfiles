e2ead2cea8fa8c45ccb52b19c4d180701ec30f5d
Jeff Ramnani <jefframnani@yahoo.com>
2012-07-01 01:51:32 -0500

    Fish: tabs sneaked into a file.

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -7,21 +7,21 @@ function fish_prompt -d "Write out the prompt"
     printf '%s(%s)%s ' (set_color blue) (basename $VIRTUAL_ENV)
   end
 
-	# Color writeable dirs green, read-only dirs red
-	if test -w "."
-		printf '%s%s' (set_color green) (prompt_pwd)
-	else
-		printf '%s%s' (set_color red) (prompt_pwd)
-	end
+  # Color writeable dirs green, read-only dirs red
+  if test -w "."
+    printf '%s%s' (set_color green) (prompt_pwd)
+  else
+    printf '%s%s' (set_color red) (prompt_pwd)
+  end
 
-	# Print git branch
-	if test -d ".git"
-		printf ' %s(%s)%s\n' (set_color blue) (parse_git_branch) (set_color normal)
+  # Print git branch
+  if test -d ".git"
+    printf ' %s(%s)%s\n' (set_color blue) (parse_git_branch) (set_color normal)
   else
     printf '\n'
-	end
+  end
 
-	printf '%s%s@%s%s $ ' (set_color normal) (whoami) (hostname|cut -d . -f 1) (set_color normal) 
+  printf '%s%s@%s%s $ ' (set_color normal) (whoami) (hostname|cut -d . -f 1) (set_color normal) 
 
 end
 
