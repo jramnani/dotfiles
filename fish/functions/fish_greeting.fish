93b877a02d4e3204f94b0860a80fe33e7f0a26c4
Jeff Ramnani <jeff@jefframnani.com>
2014-08-18 14:05:29 -0500

    fish_greeting doesn't error if executables aren't found.
    If a command substitution evaluates to nothing, then no argument is
    available for the command consuming that substitution value.
    e.g. 'test -x (which /usr/bin/bogus) ; echo $status' returns 0.
    
    Fixed by assigning the command substitution result to a local variable,
    and testing that value.
    
    Source:
    http://sourceforge.net/p/fish/mailman/message/29584569/

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -6,22 +6,26 @@ function fish_greeting -d "Set the value of fish_greeting"
 
   set GREETING (uname -srp) " -- "
 
-  if test -x /usr/bin/java
+  set -l JAVA (which java)
+  if test -x "$JAVA"
     set JAVA_VERSION (java -version 2>| awk '/java version/ { print $3 }')
     set GREETING $GREETING "Java $JAVA_VERSION, "
   end
 
-  if test -x (which python)
+  set -l PYTHON (which python)
+  if test -x "$PYTHON"
     set PYFULLVERSION (python -V 2>| awk '{print $2}')
     set GREETING $GREETING "Python $PYFULLVERSION, "
   end
 
-  if test -x (which ruby)
+  set -l RUBY (which ruby)
+  if test -x "$RUBY"
     set RUBY_VERSION (ruby --version | awk '{print $2}')
     set GREETING $GREETING "Ruby $RUBY_VERSION, "
   end
 
-  if test -x (which node)
+  set -l NODEJS (which node)
+  if test -x "$NODEJS"
     set NODE_VERSION (node --version)
     set GREETING $GREETING "Node.js $NODE_VERSION"
   end
