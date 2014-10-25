d1e9e20763c5f7e72ee953f55be182f16a74c0c2
Jeff Ramnani <jeff@jefframnani.com>
2014-10-24 19:14:23 -0500

    Only enable package archives if Emacs supports them.
    Use the same format for adding repos to the list of archives as used by
    the main repo web sites.  Easier copy pasta.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,9 +1,10 @@
-(setq package-archives '(("melpa" . "http://melpa.milkbox.net/packages/")
-			 ("org" . "http://orgmode.org/elpa/")
-			 ("gnu" . "http://elpa.gnu.org/packages/")))
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (package-initialize)
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/") t)
+  (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+  (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t))
 
-(require 'package)
-(package-initialize)
 
 ;;;;;;;;;;;;;
 ;;
