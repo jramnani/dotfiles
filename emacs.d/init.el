8923e1bb5d8ad2dd2ae24cbfedf077ffe4c0060c
Jeff Ramnani <jeff@jefframnani.com>
2014-11-02 10:39:02 -0600

    Emacs uses regexp searching by default.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -176,6 +176,12 @@
                                 (split-window-horizontally)
                                 (other-window 1)))
 
+;; Use regexp searching by default
+(global-set-key (kbd "C-s") 'isearch-forward-regexp)
+(global-set-key (kbd "C-M-s") 'isearch-forward)
+(global-set-key (kbd "C-r") 'isearch-backward-regexp)
+(global-set-key (kbd "C-M-r") 'isearch-backward)
+
 ;; org-mode key bindings need to be specified when using iTerm2
 (when (string= "iTerm.app" (getenv "TERM_PROGRAM"))
   (define-key input-decode-map "\e[1;9A" [M-up])
