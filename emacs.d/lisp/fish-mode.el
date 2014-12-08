6c85f21fe693fd6565ed82bd52a71f5338f27ca8
Jeff Ramnani <jeff@jefframnani.com>
2014-12-08 13:43:32 -0600

    A major mode to edit Fish shell scripts.
    WIP: Syntax highlightings of variable names.  Still working on
    highlighting builtin commands.

---
+++ emacs.d/lisp/fish-mode.el
@@ -0,0 +1,60 @@
+(defvar fish-builtin-commands
+  '("alias" "and"
+    "begin" "bg" "bind" "block" "break" "breakpoint" "builtin"
+    "case" "cd" "command" "commandline" "complete" "contains" "continue" "count"
+    "dirh" "dirs"
+    "echo" "else" "emit" "end" "eval" "exec" "exit"
+    "fg" "fish" "fish_config" "fish_indent" "fish_pager" "fish_prompt" "fish_right_prompt"
+    "fish_update_completions" "fishd" "for" "funced" "funcsave" "function" "functions"
+    "help" "history"
+    "if" "isatty"
+    "jobs"
+    "math" "mimedb"
+    "nextd" "not"
+    "open" "or"
+    "popd" "prevd" "psub" "pushd" "pwd"
+    "random" "read" "return"
+    "set" "set_color" "source" "status" "switch"
+    "test" "trap" "type"
+    "ulimit" "umask" "vared" "while"))
+
+(defvar fish-builtin-commands-re
+  (regexp-opt fish-builtin-commands 'words))
+
+(defvar fish-font-lock-keywords-1
+  (list
+;;   '(,(fish-builtin-commands-re) . font-lock-builtin-face)
+   '("\\$\\([[:alpha:]_][[:alnum:]_]*\\)" . font-lock-variable-name-face)))
+
+(defvar fish-mode-syntax-table
+  (let ((tab (make-syntax-table text-mode-syntax-table)))
+    (modify-syntax-entry ?\# "<" tab)
+    (modify-syntax-entry ?\n ">" tab)
+    (modify-syntax-entry ?\" "\"\"" tab)
+    (modify-syntax-entry ?\' "\"'" tab)
+    (modify-syntax-entry ?_ "_" tab)
+    (modify-syntax-entry ?$ "_" tab)
+    (modify-syntax-entry ?= "." tab)
+    (modify-syntax-entry ?& "." tab)
+    (modify-syntax-entry ?| "." tab)
+    (modify-syntax-entry ?< "." tab)
+    (modify-syntax-entry ?> "." tab)
+    (modify-syntax-entry ?- "." tab)
+    tab)
+  "Syntax table used in Fish-mode buffers.")
+
+
+;;; fish-mode.el ends here
+;;;###autoload
+(define-derived-mode fish-mode prog-mode "Fish"
+  "Major mode for editing fish shell files."
+  (setq-local font-lock-defaults '((fish-font-lock-keywords-1)))
+  (setq-local comment-start "# ")
+  (setq-local comment-start-skip "#+[\t ]*"))
+
+;;;###autoload (add-to-list 'auto-mode-alist '("\\.fish\\'" . fish-mode))
+;;;###autoload (add-to-list 'interpreter-mode-alist '("fish" . fish-mode))
+
+(provide 'fish-mode)
+
+;;; fish-mode.el ends here
