564f4045306a4eef305ae4226f9bdf9b905b4718
Jeff Ramnani <jeff@jefframnani.com>
2015-02-11 10:18:45 -0600

    Register my IRC nick after login.

--- emacs.d/.ercrc.el
+++ emacs.d/.ercrc.el
@@ -6,5 +6,20 @@
 ;; take effect.
 (erc-update-modules)
 
+;; Set my nickname
+(setq erc-nick "jramnani")
+
 ;; Hide messages that break up conversations.
 (setq erc-hide-list '("JOIN" "PART" "QUIT"))
+
+;; Identify my nickname with the IRC server
+(load "jramnani-find-password.el")
+
+(defun register-nickname-after-connect (SERVER NICK)
+  (let ((credentials (jramnani-find-password SERVER
+                                             (erc-compute-port)
+                                             NICK)))
+    (if credentials
+        (erc-message "PRIVMSG" (concat "NickServ identify " (nth 1 (credentials)))))))
+
+(add-hook 'erc-after-connect 'register-nickname-after-connect)
---
+++ emacs.d/lisp/jramnani-find-password.el
@@ -0,0 +1,19 @@
+;; https://www.gnu.org/software/emacs/manual/html_node/auth/Help-for-developers.html#Help-for-developers
+
+(defun jramnani-find-password (HOST PORT USERNAME)
+  "Find a password in Emacs auth-source.
+HOST PORT and USERNAME are all strings to use for the query.
+Return a list of strings as (USERNAME PASSWORD).
+Return nil if the entry is not found."
+  (let ((found (nth 0 (auth-source-search :host HOST
+                                          :port PORT
+                                          :user USERNAME
+                                          :require '(:user :secret)
+                                          :max 1))))
+    (if found
+        (list (plist-get found :user)
+              (let ((secret (plist-get found :secret)))
+                (if (functionp secret)
+                    (funcall secret)
+                  secret)))
+      nil)))
