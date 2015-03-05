da57ec2fd477d739a1a3597ccef4d96f4e35b504
Jeff Ramnani <jeff@jefframnani.com>
2015-03-05 13:08:50 -0600

    Solarized dark works in terminal (iTerm2).
    Fixed using code from this bug report:
    https://github.com/sellout/emacs-color-theme-solarized/issues/142

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -65,6 +65,8 @@
       ;; determine whether to use the light or dark version of the
       ;; theme.
       (set-frame-parameter nil 'background-mode 'dark)
+      (when (not (display-graphic-p))
+        (set-terminal-parameter nil 'background-mode 'dark))
       ;; Call enable-theme to pick up the change to 'background-mode.
       (enable-theme 'solarized))
     :ensure t)
