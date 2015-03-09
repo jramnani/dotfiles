f92b985d6faf341058e165a082d384c1099156e0
Jeff Ramnani <jeff@jefframnani.com>
2015-03-09 11:12:26 -0500

    Move (or rename) buffers and files together.
    Courtesy of Steve Yegge:
    https://sites.google.com/site/steveyegge2/my-dot-emacs-file

---
+++ emacs.d/vendor/steve-yegge-buffer-utils.el
@@ -0,0 +1,49 @@
+;;; steve-yegge-buffer-utils --- Summary
+;;;
+;;; Commentary:
+;; 'rename-file-and-buffer' and 'move-buffer-file' are courtesy of
+;; Steve Yegge.  Thanks, Stevey.
+;; https://sites.google.com/site/steveyegge2/my-dot-emacs-file
+;;
+;;
+;;; Code:
+
+
+(defun rename-file-and-buffer (new-name)
+  "Renames both current buffer and file it's visiting to NEW-NAME."
+  (interactive "sNew name: ")
+  (let ((name (buffer-name))
+	(filename (buffer-file-name)))
+    (if (not filename)
+	(message "Buffer '%s' is not visiting a file!" name)
+      (if (get-buffer new-name)
+          (message "A buffer named '%s' already exists!" new-name)
+        (progn
+          (rename-file name new-name 1)
+          (rename-buffer new-name)
+          (set-visited-file-name new-name)
+          (set-buffer-modified-p nil))))))
+
+
+(defun move-buffer-file (dir)
+  "Move both current buffer and file it's visiting to DIR."
+  (interactive "DNew directory: ")
+  (let* ((name (buffer-name))
+	 (filename (buffer-file-name))
+	 (dir
+          (if (string-match dir "\\(?:/\\|\\\\)$")
+              (substring dir 0 -1) dir))
+	 (newname (concat dir "/" name)))
+
+    (if (not filename)
+	(message "Buffer '%s' is not visiting a file!" name)
+      (progn
+        (copy-file filename newname 1)
+        (delete-file filename)
+        (set-visited-file-name newname)
+        (set-buffer-modified-p nil)
+        t))))
+
+
+(provide 'steve-yegge-buffer-utils)
+;;; steve-yegge-buffer-utils.el ends here
