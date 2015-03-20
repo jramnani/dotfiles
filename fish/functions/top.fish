ae5c308f3bbb034b5ffc17b9ceda7464feef8507
Jeff Ramnani <jeff@jefframnani.com>
2015-03-20 10:37:06 -0500

    Fix whitespace.

--- fish/functions/top.fish
+++ fish/functions/top.fish
@@ -1,9 +1,9 @@
 function top -d "display and update sorted information about processes"
-  if test $MYOS = "OSX"
-    # On OS X top defaults to sorting by PID descending.
-    # That is never what I want when I'm checking process usage.
-    command top -o cpu $argv
-  else
-    command top $argv
-  end
+    if test $MYOS = "OSX"
+        # On OS X top defaults to sorting by PID descending.
+        # That is never what I want when I'm checking process usage.
+        command top -o cpu $argv
+    else
+        command top $argv
+    end
 end
