93a546415f2a16daad434bbf8b1fd38c1b372504
Jeff Ramnani <jeff@jefframnani.com>
2015-03-20 10:37:56 -0500

    Top uses delta mode by default.

--- fish/functions/top.fish
+++ fish/functions/top.fish
@@ -2,7 +2,8 @@ function top -d "display and update sorted information about processes"
     if test $MYOS = "OSX"
         # On OS X top defaults to sorting by PID descending.
         # That is never what I want when I'm checking process usage.
-        command top -o cpu $argv
+        # Use delta mode because it's more useful.
+        command top -o cpu -d $argv
     else
         command top $argv
     end
