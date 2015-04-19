dae121960c7015fd9b10494aa657a6b75d572b3e
Jeff Ramnani <jeff@jefframnani.com>
2015-04-19 09:00:57 -0500

    Required by Magit upgrade.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -86,6 +86,8 @@
     :ensure t)
 
   (use-package magit
+    :init
+    (setq magit-last-seen-setup-instructions "1.4.0")
     :bind ("C-c g" . magit-status)
     :diminish magit-auto-revert-mode ;; (MRev)
     :ensure t)
