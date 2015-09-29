319fbdf6602572e2ed3a4acbc8baa42e6134df79
Jeff Ramnani <jeff@jefframnani.com>
2015-09-29 12:17:09 -0500

    Only remove one PATH element at a time.
    Whoops. Previous implementation was buggy. Just keep it simple for now.

--- fish/functions/pathrm.fish
+++ fish/functions/pathrm.fish
@@ -1,17 +1,15 @@
-function pathrm -d "Remove one or more directories from the PATH"
+function pathrm -d "Remove a directory from the PATH"
     if test (count $argv) -lt 1
-        echo "Usage: pathrm DIR [DIR ...]"
+        echo "Usage: pathrm DIR"
         exit 1
     end
 
     set -l NEW_PATH
     for p in $PATH
-        for path_to_remove in $argv
-            if test "$p" = "$path_to_remove"
-                continue
-            else
-                set NEW_PATH $NEW_PATH "$p"
-            end
+        if test "$p" = "$path_to_remove"
+            continue
+        else
+            set NEW_PATH $NEW_PATH "$p"
         end
     end
 
