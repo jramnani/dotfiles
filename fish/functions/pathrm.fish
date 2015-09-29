feff2f646980acf63114aba58f68604db8f377e1
Jeff Ramnani <jeff@jefframnani.com>
2015-09-29 12:27:29 -0500

    Simplify pathrm some more.
    Jiminy. I need to get more sleep.

--- fish/functions/pathrm.fish
+++ fish/functions/pathrm.fish
@@ -1,14 +1,14 @@
 function pathrm -d "Remove a directory from the PATH"
-    if test (count $argv) -lt 1
+    if test (count $argv) -ne 1
         echo "Usage: pathrm DIR"
-        exit 1
+        return 1
     end
 
     set -l NEW_PATH
+    set -l path_to_remove $argv[1]
+
     for p in $PATH
-        if test "$p" = "$path_to_remove"
-            continue
-        else
+        if test "$p" != "$path_to_remove"
             set NEW_PATH $NEW_PATH "$p"
         end
     end
