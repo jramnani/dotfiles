18bae2b4edabef1585bea94f970aba4765e5e0bf
Jeff Ramnani <jeff@jefframnani.com>
2015-10-12 09:56:06 -0500

    Fix indentation.

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -2,24 +2,24 @@
 #   (virtualenv) pwd (git branch)
 #   username@host $
 function fish_prompt -d "Write out the prompt"
-  # Python Virtualenv
-  if set -q VIRTUAL_ENV
-    printf '%s(%s)%s ' (set_color blue) (basename $VIRTUAL_ENV) (set_color normal)
-  end
+    # Python Virtualenv
+    if set -q VIRTUAL_ENV
+        printf '%s(%s)%s ' (set_color blue) (basename $VIRTUAL_ENV) (set_color normal)
+    end
 
-  # Color writeable dirs green, read-only dirs red
-  if test -w "."
-    printf '%s%s%s' (set_color green) (prompt_pwd) (set_color normal)
-  else
-    printf '%s%s%s' (set_color red) (prompt_pwd) (set_color normal)
-  end
+    # Color writeable dirs green, read-only dirs red
+    if test -w "."
+        printf '%s%s%s' (set_color green) (prompt_pwd) (set_color normal)
+    else
+        printf '%s%s%s' (set_color red) (prompt_pwd) (set_color normal)
+    end
 
-  # Git branch and status
-  if git rev-parse --git-dir > /dev/null 2>&1
-     printf ' (%s)' (parse_git_branch)
-  end
+    # Git branch and status
+    if git rev-parse --git-dir > /dev/null 2>&1
+        printf ' (%s)' (parse_git_branch)
+    end
 
-  printf '\n'
+    printf '\n'
 
-  printf '%s%s@%s%s $ ' (set_color normal) (whoami) (hostname|cut -d . -f 1) (set_color normal)
+    printf '%s%s@%s%s $ ' (set_color normal) (whoami) (hostname|cut -d . -f 1) (set_color normal)
 end
