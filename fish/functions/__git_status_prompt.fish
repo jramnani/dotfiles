85ecfcf8131e78822e4e26f1618ad88e8cafb9fa
Jeff Ramnani <jeff@jefframnani.com>
2015-10-16 17:48:40 -0500

    Fix fish_prompt failure in empty Git repo.
    After running 'git init' fish_prompt would fail, and print '>'. This
    happened because the command, "git diff --quiet HEAD", fails with an
    error message when in an empty repo.  There is no HEAD version to
    compare to.
    
    Fixed by not running "git diff" unless a branch exists.

--- fish/functions/__git_status_prompt.fish
+++ fish/functions/__git_status_prompt.fish
@@ -6,15 +6,17 @@ function __git_status_prompt -d "Print the status of git repository for use in t
     # it will look like: '(HEAD detached at c2814fa)'
     set -l branch (git branch --no-color ^&- | awk '/^\*/ {print $0}' | sed -e 's/\* //' | sed -e 's/[()]//g')
 
-    set_color $git_prompt_color
-    printf '%s' $branch
-    set_color normal
-
-    # Get the status of the working directory. Are there modified files?
-    git diff --quiet HEAD 2>&1
-    if test $status = 1
-        set_color red
-        printf ' Δ'
+    if test -n "$branch"
+        set_color $git_prompt_color
+        printf '%s' $branch
         set_color normal
+
+        # Get the status of the working directory. Are there modified files?
+        git diff --quiet HEAD 2>&1
+        if test $status = 1
+            set_color red
+            printf ' Δ'
+            set_color normal
+        end
     end
 end
