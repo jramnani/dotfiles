cb7fd4294c6294d438c411549e09599e26993f4e
Jeff Ramnani <jramnani@8thlight.com>
2016-01-14 09:54:52 -0600

    Add 'humane-test-output' to Clojure config.
    Thanks @jakemcc.
    http://jakemccrary.com/blog/2015/01/11/overview-of-my-leiningen-profiles-dot-clj/

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,2 +1,5 @@
-{:user {:plugins [[cider/cider-nrepl "0.10.1"]
-                  lein-pprint "1.1.2"]}}
+{:user {:dependencies [[pjstadig/humane-test-output "0.6.0"]]
+        :injections [(require 'pjstadig.humane-test-output)
+                     (pjstadig.humane-test-output/activate!)]
+        :plugins [[cider/cider-nrepl "0.10.1"]
+                  [lein-pprint "1.1.2"]]}}
