5221323418ffbd2d62fd74d41d5e3a774d9b41b3
Jeff Ramnani <jramnani@8thlight.com>
2016-01-19 11:55:59 -0600

    Fix cider-repl version and use lein-test-refresh.

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,5 +1,6 @@
 {:user {:dependencies [[pjstadig/humane-test-output "0.6.0"]]
         :injections [(require 'pjstadig.humane-test-output)
                      (pjstadig.humane-test-output/activate!)]
-        :plugins [[cider/cider-nrepl "0.10.1"]
+        :plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]
+                  [com.jakemccrary/lein-test-refresh "0.12.0"]
                   [lein-pprint "1.1.2"]]}}
