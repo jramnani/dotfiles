e8bf698b41e1f8c2878bce0f6eb7839f4ad092b0
Jeff Ramnani <jeff@jefframnani.com>
2016-06-26 13:54:16 -0500

    Remove unused git function for parsing git branches.

--- fish/functions/parse_git_branch.fish
+++
@@ -1,5 +0,0 @@
-function parse_git_branch
-         # git branch outputs lines, the current branch is prefixed with a *
-         set -l branch (git branch --color ^&- | awk '/^\*/ {print $2}')
-         echo $branch (parse_git_dirty)
-end
