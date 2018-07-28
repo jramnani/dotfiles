3144265fc5d9a41e50afb37cb1a739fe43f06bbd
Jeff Ramnani <jeff.ramnani@teacherspayteachers.com>
2018-07-28 16:53:15 -0400

    Fix startup error on iex mode hook
    alchemist is lazily loaded so not all variables exist at startup.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -162,6 +162,9 @@
                         "\\(?:^\\|\\s-+\\)\\(?:do\\)")
                    (set (make-variable-buffer-local 'ruby-end-check-statement-modifiers) nil)
                    (ruby-end-mode +1)))
+    :config
+    (add-to-list 'alchemist-iex-mode-hook
+                 (lambda () (company-mode-on)))
     :hook (elixir-mode . alchemist-mode)
     :ensure t)
 
