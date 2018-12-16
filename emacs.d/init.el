f95a40c7c0cd97f60c06799e200bcfe071539727
Jeff Ramnani <jeff.ramnani@teacherspayteachers.com>
2018-12-16 12:57:30 -0500

    Elixir format command can find the project config

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -117,6 +117,12 @@
   ;;; Elixir
   (use-package elixir-mode
     :init
+    (add-hook 'elixir-format-hook (lambda ()
+                                    (if (projectile-project-p)
+                                        (setq elixir-format-arguments
+                                              (list "--dot-formatter"
+                                                    (concat (locate-dominating-file buffer-file-name ".formatter.exs") ".formatter.exs")))
+                                      (setq elixir-format-arguments nil))))
     (add-hook 'elixir-mode-hook
               (lambda () (add-hook 'before-save-hook 'elixir-format nil t))))
 
