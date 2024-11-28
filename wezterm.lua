9d4cba3d2b0e8769700c66ad2d8c39f1d476bca0
Jeff Ramnani <jeff.ramnani@ticketmaster>
2024-11-27 16:02:13 -0800

    wezterm: update config after some use with macOS

--- wezterm.lua
+++ wezterm.lua
@@ -1,12 +1,24 @@
 local wezterm = require 'wezterm'
+
 local config = wezterm.config_builder()
 
--- Colors
+-- Color
 config.color_scheme = 'Solarized (dark) (terminal.sexy)'
 
 
 -- Font
-config.font_size = 12.0
+config.font = wezterm.font_with_fallback {
+  'Source Code Pro',
+  'Inconsolata',
+  'JetBrains Mono',
+}
+config.font_size = 14.0
 
 
+-- Scrolling
+config.scrollback_lines = 10000
+config.enable_scroll_bar = true
+
+
+-- Final
 return config
